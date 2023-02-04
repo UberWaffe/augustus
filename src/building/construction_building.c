b2fd2352d5fe89be6f0474bf27b74611ca59c431
Shawn <SCollins@oldmutual.com>
2023-02-04 02:00:15 +0200

    Fix broken codes

--- src/building/construction_building.c
+++ src/building/construction_building.c
@@ -303,19 +303,6 @@ static void add_to_map(int type, building *b, int size,
             map_tiles_update_area_roads(b->x, b->y, 4);
             building_monument_set_phase(b, MONUMENT_START);
             break;
-        case BUILDING_DEPOT:
-            switch (scenario_property_climate()) {
-            case CLIMATE_NORTHERN:
-                add_building(b, assets_get_image_id(assets_get_group_id("Areldir", "Econ_Logistics"), "Cart Depot N ON"));
-                break;
-            case CLIMATE_DESERT:
-                add_building(b, assets_get_image_id(assets_get_group_id("Areldir", "Econ_Logistics"), "Cart Depot S ON"));
-                break;
-            default:
-                add_building(b, assets_get_image_id(assets_get_group_id("Areldir", "Econ_Logistics"), "Cart Depot C ON"));
-                break;
-            }
-            break;
     }
     map_routing_update_land();
     map_routing_update_walls();
